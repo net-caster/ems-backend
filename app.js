53db6cb7a6ad7f320381429f653a216314de6bf0
Tyler McGinnis <mcginnis.tyler@gmail.com>
2019-11-14 04:08:15 +0000

    removed unused code

--- app.js
+++ app.js
@@ -1,93 +1,95 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const path = require('path');
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
-const helmet = require('helmet');
-const compression = require('compression');
-const cors = require('cors');
-const moment = require('moment');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const compression = require("compression");
+const cors = require("cors");
 
-const sequelize = require('./api/database/mysql-db');
+const sequelize = require("./api/database/mysql-db");
 
 const port = process.env.PORT || 3000;
 
-const User = require('./api/models/User');
-const Employee = require('./api/models/Employee');
-const WorkDay = require('./api/models/WorkDay');
-const DateDim = require('./api/models/DateDim');
+const User = require("./api/models/User");
+const Employee = require("./api/models/Employee");
+const WorkDay = require("./api/models/WorkDay");
 
-const userRoutes = require('./api/routes/user');
+const userRoutes = require("./api/routes/user");
 
 app.use(cookieParser());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(helmet());
 app.use(compression());
 
 app.use(
-	cors({
-		origin: process.env.URL,
-		credentials: true
-	})
+  cors({
+    origin: process.env.URL,
+    credentials: true
+  })
 );
 
-Employee.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+Employee.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Employee);
-WorkDay.belongsTo(Employee, { constraints: true, onDelete: 'CASCADE' });
+WorkDay.belongsTo(Employee, { constraints: true, onDelete: "CASCADE" });
 Employee.hasMany(WorkDay);
-WorkDay.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+WorkDay.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(WorkDay);
 
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', process.env.URL);
-	res.header('Access-Control-Allow-Credentials', true);
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-	if (req.method === 'OPTIONS') {
-		res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-		return res.status(200).json({});
-	}
-	next();
+  res.header("Access-Control-Allow-Origin", process.env.URL);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    );
+    return res.status(200).json({});
+  }
+  next();
 });
 
 app.use(userRoutes);
 
 (async () => {
-	try {
-		// await sequelize.sync({ force: true });
+  try {
+    // await sequelize.sync({ force: true });
 
-		await sequelize.sync();
+    await sequelize.sync();
 
-		await app.listen(port);
+    await app.listen(port);
 
-		console.log('');
-		console.log('Connection established...');
-		console.log('Server is running on port: ' + port);
-	} catch (err) {
-		console.log(err);
-	}
+    console.log("");
+    console.log("Connection established...");
+    console.log("Server is running on port: " + port);
+  } catch (err) {
+    console.log(err);
+  }
 })();
 
 app.use((req, res, next) => {
-	res.status(200).json({
-		pageTitle: 'Index',
-		path: '/'
-	});
+  res.status(200).json({
+    pageTitle: "Index",
+    path: "/"
+  });
 });
 
 app.use((req, res, next) => {
-	res.status(404).json({
-		pageTitle: 'Page Not Found!',
-		path: '/404'
-	});
+  res.status(404).json({
+    pageTitle: "Page Not Found!",
+    path: "/404"
+  });
 });
 
 app.use((error, req, res, next) => {
-	res.status(error.status || 500).json({
-		pageTitle: 'Server Error!',
-		path: '/500'
-	});
+  res.status(error.status || 500).json({
+    pageTitle: "Server Error!",
+    path: "/500"
+  });
 });
